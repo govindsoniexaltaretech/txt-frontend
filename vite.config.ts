6e617ac04668525e718793d4e38e2e027cfd42e5
govindsoniexaltaretech <govind.soni@exaltaretech.com>
2024-09-04 18:51:47 +0530

    Live server code

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/",
+//   build: {
+//     chunkSizeWarningLimit: 3000,
+//   },
+//   server: {
+//     open: true, // This will open the browser automatically
+//   },
+// })
+
+
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/",
-  build: {
-    chunkSizeWarningLimit: 3000,
-  },
   server: {
-    open: true, // This will open the browser automatically
+    open: true, 
   },
-})
+});
