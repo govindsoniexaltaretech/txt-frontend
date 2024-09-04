7dd9f8a8a2eee8c05bd58761ffea9fc10b3aefc1
govindsoniexaltaretech <govind.soni@exaltaretech.com>
2024-09-04 19:05:59 +0530

    Added Base Url

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,13 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-
-// export default defineConfig({
-//   plugins: [react()],
-//   base: "/",
-//   build: {
-//     chunkSizeWarningLimit: 3000,
-//   },
-//   server: {
-//     open: true, // This will open the browser automatically
-//   },
-// })
-
-
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  server: {
-    open: true, 
+  plugins: [react()],
+  base: "/txt-frontend",
+  build: {
+    chunkSizeWarningLimit: 3000,
   },
-});
+  server: {
+    open: true, // This will open the browser automatically
+  },
+})
