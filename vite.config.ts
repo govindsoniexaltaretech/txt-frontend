36809a3293f11d59192ae6610b7cd0cfa4fdfc61
govindsoniexaltaretech <govind.soni@exaltaretech.com>
2024-09-04 18:47:45 +0530

    Update Server Auto Live Code

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: "/users",
-  
+  base: "/",
   build: {
     chunkSizeWarningLimit: 3000,
+  },
+  server: {
+    open: true, // This will open the browser automatically
   },
 })
