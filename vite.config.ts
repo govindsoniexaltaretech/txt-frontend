019a2630652729799c3d2fee691f49a4fe03baf8
govindsoniexaltaretech <govind.soni@exaltaretech.com>
2024-09-05 09:49:11 +0530

    Base URL Update

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/txt-frontend",
+  base: "/",
   build: {
     chunkSizeWarningLimit: 3000,
   },
@@ -11,3 +11,4 @@ export default defineConfig({
     open: true, // This will open the browser automatically
   },
 })
+
