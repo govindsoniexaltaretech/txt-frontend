1adf30821710f624b94a7089be0639b143da9ad0
govindsoniexaltaretech <govind.soni@exaltaretech.com>
2024-09-06 12:37:45 +0530

    Update Data Changes and Base URL

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: "/",
   build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        nested: resolve(__dirname, 'src/main.tsx'),
-      },
-    },
+    chunkSizeWarningLimit: 3000,
   },
   server: {
     open: true, // This will open the browser automatically
